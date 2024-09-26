9863deac30b93c3d18cb7b5378ffb18dc6639e72
thianesh-03 <thianesh2003@gmail.com>
2024-09-26 12:45:38 +0530

    about page done

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,15 @@
-import {AboutImg} from '../Assets/About.png';
+import aboutImg from "../Assets/About.png";
 
 export default function About() {
-    return <section>
-        <div>
-            <img src={AboutImg}></img>
+    return <section className="flex flex-col md:flex-row bg-primary md:py-32 px-5">
+        <div className="mt-10 py-5 md:w-1/3 md:ml-24">
+            <img src={aboutImg} alt="My portrait"></img>
         </div>
-        <div>
-            <h1>About Me</h1>
-            <p>Some information about myself</p>
+        <div className="md:w-1/2 flex justify-center">
+            <div className="flex flex-col justify-center ml-10">
+                <h1 className="text-4xl text-white border-b-2 mb-4 w-[170px] font-bold">About Me</h1>
+                <p className="text-white">Innovative software engineer with a strong foundation in programming and problem-solving. Proficient in multiple programming languages and development tools. Experienced in Figma, Structured Query Language and other programming and designing platform. Passionate about staying upto-date with emerging technologies and collaborating with cross-functional teams to deliver cutting-edge products.</p>
+            </div>
         </div>
     </section>
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -10,14 +10,13 @@ export default function Hero() {
   return (
     <section className="flex flex-col md:flex-row px-5 md:py-32 bg-secondary justify-center">
       <div className="md:w-1/2">
-        <h1 className="text-white text-6xl py-16 font-hero-font md:ml-24 ">
+        <h1 className="text-white text-6xl mt-10 font-hero-font md:ml-24 ">
           {" "}
-          {/* Reduced py-20 to py-16 */}
           Hi,
           <br /> I'm Thianesh Govindasamy
           <p className="text-2xl">I'm a Full-Stack developer</p>
         </h1>
-        <div className="flex md:ml-24">
+        <div className="flex md:ml-24 py-10">
           <a href="#" className="pr-5 hover:text-white">
             <AiOutlineMail size={40} />
           </a>
@@ -32,7 +31,7 @@ export default function Hero() {
           </a>
         </div>
       </div>
-      <img className="md:w-1/3 md:mr-20" src={HeroImg} />
+      <img className="md:w-1/3 md:mr-20 hidden lg:block" src={HeroImg} alt="portrait" />
     </section>
   );
 }
