e9fa71a497194c370da713bf0ffa6297433b13f6
thianesh-03 <thianesh2003@gmail.com>
2024-10-12 15:19:50 +0530

    altered the about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <div className="md:w-1/2 flex justify-center">
             <div className="flex flex-col justify-center ml-10">
                 <h1 className="text-4xl text-white border-b-2 mb-4 w-[170px] font-bold">About Me</h1>
-                <p className="text-white pb-10 md:pb-0">Innovative software engineer with a strong foundation in programming and problem-solving.<br/><br/> Proficient in multiple programming languages and development tools.<br/><br/> Experienced in Figma, Structured Query Language and other programming and designing platform.<br/><br/> Passionate about staying upto-date with emerging technologies and collaborating with cross-functional teams to deliver cutting-edge products.</p>
+                <p className="text-white pb-10 md:pb-0">Innovative software engineer with a strong foundation in programming and problem-solving.<br/><br/> Proficient in multiple programming languages and development tools.<br/><br/> Experienced in Figma, Structured Query Language, MERN stack and other programming and designing platform.<br/><br/> Passionate about staying upto-date with emerging technologies and collaborating with cross-functional teams to deliver cutting-edge products.</p>
             </div>
         </div>
     </section>
